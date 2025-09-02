121ab57da65c329ee6e3688f906c9400da79a58b
kazakong <kazakong@naver.com>
2025-09-02 22:10:51 +0900

    Review

--- src/day2/ccc18j1.kt
+++ src/day2/ccc18j1.kt
@@ -4,17 +4,35 @@ fun main(){
     val input = System.`in`.bufferedReader()
     val output = System.out.bufferedWriter()
     val n1 = input.readLine()
-    if(n1.length !=1 || in "0123456789")throw Throwable ("invalid int n1 :$n1")
+    if(n1.length != 1 || in "0123456789")throw Throwable ("invalid int n1 :$n1")
     val n2 = input.readLine()
-    if(n2.length !=1 || in "0123456789")throw Throwable ("invalid int n2 :$n2")
+    if(n2.length != 1 || in "0123456789")throw Throwable ("invalid int n2 :$n2")
     val n3 = input.readLine()
-    if(n3.length !=1 || in "0123456789")throw Throwable ("invalid int n3 :$n3")
+    if(n3.length != 1 || in "0123456789")throw Throwable ("invalid int n3 :$n3")
     val n4 = input.readLine()
-    if(n4.length !=1 || in "0123456789")throw Throwable ("invalid int n4 :$n4")
+    if(n4.length != 1 || in "0123456789")throw Throwable ("invalid int n4 :$n4")
 
-    val result = if (n1 in "89" && n4 in "89" && n2==n3) "ignore" else "answer"
+    val result = if (n1 in "89" && n4 in "89" && n2 == n3) "ignore" else "answer"
+    output.write(result)}
 
-}
-}
-}
-}
+
+// 내가 작성 했던 코드
+//*fun main() {
+//    val telenumber = readLine()!!
+//
+//
+//    if (telenumber.length == 4 && telenumber.all { it.isDigit() }) {
+//        val firstDigit = telenumber[0]
+//        val secondDigit = telenumber[1]
+//        val thirdDigit = telenumber[2]
+//        val lastDigit = telenumber[3]
+//
+//
+//        if ((firstDigit == '8' || firstDigit == '9') && secondDigit == thirdDigit && (lastDigit == '8' || lastDigit == '9')) {
+//            println(telenumber)
+//        } else {
+//            println("ignore")
+//        }
+//    } else {
+//        println("answer")
+//    }
--- src/day2/ccc19j1.kt
+++ src/day2/ccc19j1.kt
@@ -26,4 +26,30 @@ fun main(){
         output.write(result)
     }
 
+//내가 작성 했던 코드
+//*
+//package s02
+
+//fun main() {
+
+  //  val Applethree = readLine()!!.toInt()*3
+    //val Appletwo = readLine()!!.toInt() *2
+    //val Appleone = readLine()!!.toInt() *1
+    //val Bananathree = readLine()!!.toInt()*3
+    //val Bananatwo = readLine()!!.toInt()*2
+    //val Bananaone = readLine()!!.toInt()*1
+    //val scoreA = ( Applethree + Appletwo +  Appleone)
+    //val scoreB = (Bananathree +  Bananatwo +  Bananaone)
+
+    //if (scoreA > scoreB) {
+    //    println('A')
+    //} else if (scoreB > scoreA) {
+    //    println('B')
+    //} else {
+    //    println('T')
+    //}
+//}
+
+
+
 
